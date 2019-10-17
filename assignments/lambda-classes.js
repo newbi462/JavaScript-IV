a0e3b12e92d5ff8444f9b29a333bdfdd9f295003
newbi462 <isthisemailfree@gmail.com>
2019-10-17 15:59:34 -0400

    Other classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,3 +26,39 @@ class Instructor extends Person {
     console.log(`${studentObj.name} receives a perfect score on ${subjectString}`);
   }
 }
+
+
+class Student extends Person {
+  constructor(name,age,location,previousBackground,className,favSubjects) {
+    super(name,age,location); // links to above Person class and inharits funtion actiong as proto
+    this.previousBackground = previousBackground;
+    this.className = className;
+    this.favSubjects = favSubjects;
+  }
+  listsSubjects() {
+    for (var i = 0; i < this.favSubjects.length; i++) {
+      console.log(this.favSubjects[i]);
+    }
+  }
+  PRAssignment(subjectString) { // ya this should not have been CAPS, but orders are orders
+    console.log(`${this.name} has submitted a PR for ${subjectString}`);
+  }
+  sprintChallenge(subjectString) {
+    console.log(`${this.name} has begun sprint challenge on ${subjectString}`);
+  }
+}
+
+
+class ProjectManager extends Instructor {
+  constructor(name,age,location,specialty,favLanguage,catchPhrase,gradClassName,favInstructor) {
+    super(name,age,location,specialty,favLanguage,catchPhrase); // links to above Instructor class and inharits funtion actiong as proto
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+  }
+  standUp(slackchannelStrng) {
+    console.log(`${this.name} announces to ${slackchannelStrng}, @channel standy times!​​​​​`);
+  }
+  debugsCode(studentObj,subjectString) {
+    console.log(`${this.name} debugs ${studentObj.name}'s code on ${subjectString}`);
+  }
+}
